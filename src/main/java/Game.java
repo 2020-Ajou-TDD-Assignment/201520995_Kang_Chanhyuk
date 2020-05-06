948a101af2102ed07b5c941ce8694f5f35d5f1f0
rkdcksgur <rkdcksgur@ajou.ac.kr>
2020-05-06 16:14:10 +0900

    the third test

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -12,15 +12,39 @@ public class Game {
         int score = 0;
         int frameindex = 0;
         for(int frame = 0; frame < 10; frame++){
-            if (isSpare(frameindex)){
-                score += 10 + rolls[frameindex + 2];
+            if (isStrike(frameindex))
+            {
+                score = 10 + strikeBonus(frameindex);
+                frameindex++;
+            }
+            else if (isSpare(frameindex)){
+                score += 10 + spareBonus(frameindex);
                 frameindex += 2;
             } else {
-                score += rolls[frameindex] + rolls[frameindex + 1];
+                score += sumOfBallsInFrame(frameindex);
                 frameindex += 2;
             }
         }
         return score;
+    }
+
+    private int sumOfBallsInFrame(int frameindex) {
+        return rolls[frameindex] + rolls[frameindex + 1];
+    }
+
+    private boolean isStrike(int frameindex) {
+        return rolls[frameindex] == 10;
+    }
+
+    private int strikeBonus(int frameindex) {
+        return
+                rolls[frameindex+1] +
+                rolls[frameindex+2];
+
+    }
+
+    private int spareBonus(int frameindex){
+        return rolls[frameindex + 2];
     }
 
     private boolean isSpare(int frameindex) {
